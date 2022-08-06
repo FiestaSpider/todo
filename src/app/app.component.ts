6ac8cee6794b54fa64b5f5fc41d4691c2780590b
Luis <luismacario_smash@homail.com>
2022-08-05 21:12:32 -0600

    validacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,41 @@
 import { Component } from '@angular/core';
 
+class Todo {
+  description = '';
+  completed = false;
+  date = '';
+  editing = true;
+  formatDate() {
+    return new Date(this.date + ' 00:00:00').toLocaleDateString('es-MX', { dateStyle: 'full' });
+  }
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  todos = [] as {
-    description: string;
-    completed: boolean;
-    date: string;
-    remove: () => void;
-  }[];
+  todos = [
+    Object.assign(new Todo(), {
+      description: 'Check in',
+      completed: true,
+      date: '2022-08-01',
+      editing: false
+    })
+  ] as Todo[];
   addTodo() {
-    const todo = {
-      description: '',
-      completed: false,
-      date: '',
-      remove: () => {
-        this.todos.splice(this.todos.indexOf(todo), 1);
-      }
-    };
-    this.todos.push(todo)
+    this.todos.push(new Todo());
+  }
+  cancelTodo(old: Todo) {
+    old.editing = false;
+  }
+  updateTodo(old: Todo, next: {
+    description: string,
+    completed: boolean,
+    date: string
+  }) {
+    Object.assign(old, next);
+    old.editing = false;
   }
 }
--- src/app/todo.component.ts
+++ src/app/todo.component.ts
@@ -1,15 +1,24 @@
-import { Component, Input } from '@angular/core';
-
-function Noop() {}
-
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent {
-  @Input() description = '';
-  @Input() completed = false;
-  @Input() date = '';
-  @Input() remove = Noop;
+  @Input() description!: string;
+  @Input() completed!: boolean;
+  @Input() date!: string;
+  @Output() todoUpdateEvent = new EventEmitter<{
+    description: string,
+    completed: boolean,
+    date: string
+  }>();
+  @Output() todoCancelEvent = new EventEmitter();
+  cancelTodo() {
+    this.todoCancelEvent.emit();
+  }
+  updateTodo() {
+    const { description, completed, date } = this;
+    this.todoUpdateEvent.emit({ description, completed, date });
+  }
 }
